83a77dc9a0d1178df6c0fa2d3cf7ec0edfa255da
Snehalmiraje <47252937+Snehalmiraje@users.noreply.github.com>
2021-08-02 12:17:45 +0530

    Add files via upload

---
+++ spring security/Q2/SpringSecurityAppCustomloginpage.java
@@ -0,0 +1,13 @@
+package com.springsecurity.demo.springSecurityExmaple.Q2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAppCustomloginpage {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAppCustomloginpage.class, args);
+	}
+
+}
